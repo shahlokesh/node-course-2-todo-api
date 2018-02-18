2b0c109bc36e5a10a4a01b60f529e43c9a524ee1
Lokesh Shah <shahlokesh619@gmail.com>
2018-02-18 22:43:48 +0530

    update resource

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+const _ = require('lodash');
+
 var express = require('express');
 var bodyParser = require('body-parser');
 var {ObjectID} = require('mongodb');
@@ -55,6 +57,29 @@ app.delete('/todos/:id', function(req, res){
      console.log('RemoveById', doc);
      return res.send({doc});
    }).catch((e) => res.status(400).send());
+});
+
+app.patch('/todos/:id',function(req, res){
+  var id = req.params.id;
+  var body =  _.pick(req.body, ['text', 'completed']);
+  if(!ObjectID.isValid(id)){
+   return res.status(404).send();
+   }
+   if(_.isBoolean(body.completed) && body.completed){
+     body.completedAt = new Date().getTime();
+   }else{
+     body.completed = false;
+     body.completedAt = null;
+   }
+
+   Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then(function(todo){
+     if(!todo){
+       return res.status(404).send();
+     }
+     res.send({todo});
+   }).catch((e)=> {
+ res.status(400).send();
+})
 });
 
 app.listen(port, function(){
