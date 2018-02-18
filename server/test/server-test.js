83fa840b22e550a758c44d8b508a0b82cb37792e
Lokesh Shah <shahlokesh619@gmail.com>
2018-02-18 13:02:24 +0530

    add to test suite

--- server/test/server-test.js
+++ server/test/server-test.js
@@ -4,8 +4,17 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+const todos = [{
+  text: 'First Test todo'
+},{
+  text: 'Second test todo'
+}
+];
+
 beforeEach(function(done){
-  Todo.remove({}).then(() => done());
+  Todo.remove({}).then(() => {
+    return Todo.insertMany(todos);
+  }).then(() => done());
 });
 
 describe('POST /todos', function(){
@@ -24,7 +33,7 @@ describe('POST /todos', function(){
         done(err);
       }
 
-      Todo.find().then(function(todos){
+      Todo.find({text}).then(function(todos){
         expect(todos.length).toBe(1);
         expect(todos[0].text).toBe(text);
         done();
@@ -43,11 +52,21 @@ it('should not be created', function(done){
       return done(err);
     }
     Todo.find().then(function(todos){
-      expect(todos.length).toBe(0);
+      expect(todos.length).toBe(2);
       done();
     }).catch((e) => done(e))
   });
 });
+});
 
-
+describe('GET /todos', ()=>{
+  it('should get all todos', (done)=>{
+    request(app)
+    .get('/todos')
+    .expect(200)
+    .expect(function(res){
+      expect(res.body.todos.length).toBe(2)
+    })
+    .end(done());
+  });
 });
